36c39755c0c16f66b5b0ab2ea9f042132a7a7180
cipher <taejin.kim@lgcns.com>
2024-11-29 11:55:28 +0900

    update

--- associate-instance-profile.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-instance_id=$1
-aws ec2 associate-iam-instance-profile --instance-id $instance_id --iam-instance-profile Name=SnowAmazonSSMForInstancesProfile --profile sts
--- create-association.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws ssm create-association \
-    --name "AWS-GatherSoftwareInventory" \
-    --targets "Key=InstanceIds,Values=*" \
-    --association-name "SnowInventoryAssociation" \
-    --schedule-expression "rate(3 days)" \
-    --region ap-northeast-2 --profile sts
